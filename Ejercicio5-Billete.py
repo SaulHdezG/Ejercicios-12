67eaf5555f02f4d2d20d8359bd00f05154db59d6
SaulHdezG <154381639+SaulHdezG@users.noreply.github.com>
2023-12-20 19:26:26 -0600

    Update Ejercicio5-Billete.py

--- Ejercicio5-Billete.py
+++ Ejercicio5-Billete.py
@@ -1,8 +1,18 @@
 #Dado un billete de $x, ¿cuáles son los personajes o hechos históricos que
 #aparecen en su anverso?
-
+#Algoritmo Billete
 billete = input("Ingrese la denominacion del billete")
-if billete == 20:
-    imagen = 1
-
-print(billete)
+if int(billete) == 20:
+    print("Aparece el Bicentenario de Independencia")
+elif int(billete) == 50:
+    print("Aparece un Ajolote")
+elif int(billete) == 100:
+    print("Aparece Sor Juana Ines de la Cruz")
+elif int(billete) == 200:
+    print("Aparece Miguel Hidalgo y Mario Morelos")
+elif int(billete) == 500:
+    print("Aparece Benito Juarez")
+elif int(billete) == 1000:
+    print("Aparece Francisco I. Madero")
+else:
+    print("Solo billetes existentes")
